6e23f4949bf7781c7e0e969ed8807b47bc37f552
Maria Glazova <m_glazova@yahoo.com>
2019-02-05 09:55:34 +0300

    new project

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+/*
+1. Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна.
+Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели.
+Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter.
+При «отсылке» сообщение перекидывается из нижнего поля в центральное.
+ */
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        TextArea textArea = new TextArea();
+        TextField textField=new TextField();
+
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+        root.setSpacing(5);
+
+        primaryStage.setTitle("Чат");
+
+        Button btn = new Button();
+        btn.setText("Отправить");
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textArea.appendText(textField.getText()+"\n");
+                textField.setText("");
+            }
+        });
+
+        root.getChildren().add(textArea);
+        root.getChildren().add(textField);
+        root.getChildren().add(btn);
+
+        Scene scene = (new Scene(root, 300, 275));
+
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER)  {
+                    textArea.appendText(textField.getText()+"\n");
+                    textField.setText("");
+                }
+            }
+        });
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
